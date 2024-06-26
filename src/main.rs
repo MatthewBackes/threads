761d8564c28d7271cab6a3ac4d63ae0509c6272f
Strom-Cuzewon <mbackes5052@gmail.com>
2024-06-26 18:26:21 -0500

    Testing with shared state concurrency

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::thread;
 use std::time::Duration;
 use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
+use std::rc::Rc;
 fn main() {
     let handle = thread::spawn(|| {
         for i in 1..10 {
@@ -59,4 +61,24 @@ fn main() {
     for received in rx{
         println!("Got: {received}");
     }
+
+    let counter = Arc::new(Mutex::new(0));
+
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
