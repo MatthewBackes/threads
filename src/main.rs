33a9ed33cfc6104aec6121e1ae40ebacbb968191
Strom-Cuzewon <mbackes5052@gmail.com>
2024-06-26 17:37:00 -0500

    Testing using threads and currents.

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use std::thread;
+use std::time::Duration;
+use std::sync::mpsc;
+fn main() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {i} from the spawned thread!");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {i} from main thread!");
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    //Calling .join makes main thread wait until the handle thread is complete.
+    handle.join().unwrap(); 
+
+    let v = vec![1, 2, 3];
+
+    //Move keywoard forces the closure to take ownership of value, rather than borrow.
+    let handle2 = thread::spawn( move || {
+        println!("Here's a vector: {v:?}");
+    });
+
+    handle2.join().unwrap();
+
+    let (tx, rx) = mpsc::channel();
+    let tx1 = tx.clone();
+
+    thread::spawn(move || {
+        let vals = vec![String::from("hi"),
+                                    String::from("from"),
+                                    String::from("the"),
+                                    String::from("thread"),
+                                ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+
+    for received in rx{
+        println!("Got: {received}");
+    }
+}
